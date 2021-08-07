f223f089d505e4df5b36b4b6fc9acaf7cd5f682c
Caleb-Irwin <56744087+Caleb-Irwin@users.noreply.github.com>
2021-08-06 23:14:02 -0600

    gets uuid, color. Displays them

--- web/ts/index.ts
+++ web/ts/index.ts
@@ -1,4 +1,20 @@
 import axios from "axios";
-axios.get("/api/ping").then((res) => {
-  console.log(res.data);
-});
+
+let uuid = undefined;
+let color = undefined;
+
+axios
+  .get("/api/newUser")
+  .then((res) => {
+    console.log(res.data);
+    uuid = res.data.uuid;
+    color = res.data.color;
+    document.getElementById("body").style.backgroundColor = "#" + color;
+    document.getElementById(
+      "user"
+    ).innerText = `UUID: "${uuid}" Color: "#${color}"`;
+  })
+  .catch((err) => {
+    console.log(err);
+    document.getElementById("user").innerText = "Error: " + err;
+  });
